8b6302a57d6a3428daa98e2181900c110c5cb3ef
Pat Martin <Pat.Martin@z.co.nz>
2019-11-25 10:49:49 +1300

    Added files to support Log Shipping and SSIS/SSRS usage.

---
+++ GetSSISConnections.sql
@@ -0,0 +1,15 @@
+USE SSISDB
+
+SELECT pr.name AS [ProjectName]
+    , pr.description AS [ProjectDescription]
+    , pr.last_deployed_time AS [ProjectLastValidated]
+    , pr.validation_status AS [ProjectValidationStatus]
+    , op.object_name AS [PackageName]
+    , op.design_default_value AS [DefaultConnectionString]
+FROM [internal].[object_parameters] op
+INNER JOIN [internal].[projects] pr
+ON pr.project_id = op.project_id
+AND pr.object_version_lsn = op.project_version_lsn
+WHERE (op.parameter_name LIKE '%.ConnectionString%'
+OR  op.parameter_name LIKE '%ServerName%')
+AND convert(varchar(max),op.design_default_value) like '%P06%'
